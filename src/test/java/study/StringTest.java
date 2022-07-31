4b9f420f8a1ef7f99a0a1f3b7371ee4594f379ed
nayoon <belloyv@gmail.com>
2022-07-31 21:24:14 +0900

    StringTest testcode

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,60 @@
 package study;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void split_have_no_delimiter() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1, 2)";
+        actual = actual.substring(1, actual.length() - 1);
+        assertThat(actual).isEqualTo("1, 2");
+    }
+
+    @Test
+    @DisplayName("chatAt Non Exception")
+    void charAt() {
+        char actual = "012345".charAt(5);
+        assertThat(actual).isEqualTo('5');
+    }
+
+    @Test
+    @DisplayName("charAt assertThatThrownBy")
+    void charAt_ExceptionTest1() {
+        String actual = "12345";
+        assertThatThrownBy(() -> {
+            actual.charAt(5); 
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+        .hasMessageContaining("String index out of range: " + actual.length());
+    }
+
+    @Test
+    @DisplayName("charAt assertThatExceptionOfType")
+    void charAt_ExceptionTest2() {
+        String actual = "12345";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+          .isThrownBy(() -> {
+              actual.charAt(5);
+        }).withMessageMatching("String index out of range: " + actual.length());
     }
 }
